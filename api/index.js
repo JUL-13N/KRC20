0ddf4a7797f17ece32172e7c72bd41c24d5e4dd4
Julien <52903784+JUL-13N@users.noreply.github.com>
2025-07-21 21:14:13 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -118,7 +118,6 @@ export default async function handler(req, res) {
             <strong>Example:</strong><br>
             <a href="/api?token=nacho">/api?token=nacho</a>
         </div>
-        <div style="margin-top: 14px;"></div>
         
         <h3>Get Total Token Supply</h3>
         <div class="endpoint">GET /api/total?token={ticker}</div>
@@ -127,7 +126,6 @@ export default async function handler(req, res) {
             <strong>Example:</strong><br>
             <a href="/api/total?token=nacho">/api/total?token=nacho</a>
         </div>
-        <div style="margin-top: 14px;"></div>
         
         <h3>Get Circulating Supply</h3>
         <div class="endpoint">GET /api/circulating?token={ticker}</div>
@@ -136,7 +134,6 @@ export default async function handler(req, res) {
             <strong>Example:</strong><br>
             <a href="/api/circulating?token=nacho">/api/circulating?token=nacho</a>
         </div>
-        <div style="margin-top: 14px;"></div>
         
         <h3>Get Unlocked Circulating Supply</h3>
         <div class="endpoint">GET /api/unlocked-circulating?token={ticker}</div>
